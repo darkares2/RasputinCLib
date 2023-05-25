f003d67a7d7a077b49c0ef625c443c2156e3ce56
rasputin <thomas.1.andersen@gmail.com>
2023-05-25 15:20:39 +0200

    Added some bit counting from hackers delight

--- main.c
+++ main.c
@@ -3,6 +3,19 @@
 #include "rasp_mem.h"
 #include "rasp_hash.h"
 
+// Count the number of set bits in a 32-bit integer
+int pop(unsigned x) {
+    x = x - ((x >> 1) & 0x55555555);
+    x = (x & 0x33333333) + ((x >> 2) & 0x33333333); x = (x + (x >> 4)) & 0x0F0F0F0F;
+    x = x + (x >> 8);
+    x = x + (x >> 16);
+    return x & 0x0000003F;
+}
+
+void fun() {
+    int x = pop(0x0F0AA00F);
+    printf("x: %d\n", x);
+}
 
 int main() {
     void *buf = malloc(500);
@@ -24,5 +37,9 @@ int main() {
     free(buf);
     rasp_debug_memory_print();
 
+    fun();
+
     return 0;
 }
+
+
